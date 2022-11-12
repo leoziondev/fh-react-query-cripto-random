2aaf170c80eb99b0ac164b23edc86a2f27eb729b
LeoZion <leozionsuporte@gmail.com>
2022-11-12 09:40:59 -0300

    start template

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="bg-slate-900 text-slate-300 flex items-center justify-center min-h-screen">
+      <h1>Hello React</h1>
+    </div>
+  )
+}
